5ad57b7ae1f5cf4d9a5300335a239fbbed1bcd2e
diah0 <halimatus833@gmail.com>
2023-05-20 12:05:57 +0800

    Buat Output

--- InputOutput.java
+++ InputOutput.java
@@ -6,5 +6,6 @@ public class InputOutput {
         String nama;
         System.out.println("Masukan nama anda : ");
         nama = scanner.nextLine();
+        System.out.println("Hai, "+nama);
     }
 }
